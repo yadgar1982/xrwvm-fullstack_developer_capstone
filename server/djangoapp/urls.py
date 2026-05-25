d7f2027222f4cb23583aa3af8ad24a43b68ff258
Zabeehullah Yadgar <yadgar1982@gmail.com>
2026-05-25 00:26:35 -0400

    register carMake and carModel

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,6 +16,9 @@ urlpatterns = [
     # path for logout
     path(route='register', view=views.registration, name='register'),
 
+    # path for get_car 
+    path(route='get_cars/', view=views.get_cars, name ='getcars'),
+
     # path for dealer reviews view
 
     # path for add a review view
