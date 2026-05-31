4e90b8c303a67e1db9a8419786f809dc399b9cda
Zabeehullah Yadgar <yadgar1982@gmail.com>
2026-05-31 12:17:20 -0400

    Fix populate.py indentation

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -114,7 +114,7 @@ def initiate():
         # Add more CarModel instances as needed
     ]
 
-        for data in car_model_data:
+    for data in car_model_data:
         CarModel.objects.create(
             name=data["name"],
             car_make=data["car_make"],
