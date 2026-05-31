2b8c865d33e70dea3871492abf2f3527ee935808
Zabeehullah Yadgar <yadgar1982@gmail.com>
2026-05-31 12:43:19 -0400

    Fix djangoproj urls flake8 issues

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -26,9 +26,14 @@ urlpatterns = [
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
-
+    path(
+        'dealer/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
+    ),
+    path(
+        'postreview/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
+    ),
 
 
     path('djangoapp/', include('djangoapp.urls')),
