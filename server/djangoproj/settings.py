d97c9998f33a66851a2386dfdc10e3deb4b4bf64
Zabeehullah Yadgar <yadgar1982@gmail.com>
2026-05-31 12:52:04 -0400

    Fix remaining settings.py flake8 issues

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://studyyadgar-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    (
+        'https://studyyadgar-8000.theianext-1-labs-prod-'
+        'misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    ),
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://studyyadgar-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    (
+        'https://studyyadgar-8000.theianext-1-labs-prod-'
+        'misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    ),
 ]
 
 REST_FRAMEWORK = {
@@ -100,8 +106,10 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator'
+),
     },
     {
         'NAME':
