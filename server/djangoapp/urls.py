a6fde490306de96907f7364c84ee9a3a83cfe207
Zabeehullah Yadgar <yadgar1982@gmail.com>
2026-05-31 12:38:46 -0400

    Fix urls and restapis flake8 issues

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,35 +6,37 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-
     # path for login
     path(route='login', view=views.login_user, name='login'),
-    
+
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
 
-    # path for logout
+    # path for get_car
     path(route='get_cars/', view=views.get_cars, name='getcars'),
 
-    # path for get_car
-    path(route='get_cars/', view=views.get_cars, name ='getcars'),
-
     # path for dealer reviews view
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(
-    route='get_dealers/<str:state>',
-    view=views.get_dealerships,
-    name='get_dealers_by_state'
+        route='get_dealers/',
+        view=views.get_dealerships,
+        name='get_dealers'
     ),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(
-    route='dealer/<int:dealer_id>',
-    view=views.get_dealer_details,
-    name='dealer_details'
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    path(
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details'
+    ),
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_reviews'
     ),
 
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
