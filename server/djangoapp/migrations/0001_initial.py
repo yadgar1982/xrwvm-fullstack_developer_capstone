df7cb96b46e57d25925dae9bb4447282927f108c
Zabeehullah Yadgar <yadgar1982@gmail.com>
2026-05-31 13:06:04 -0400

    Fix migration flake8 line length issues

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -16,20 +16,63 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarMake',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('description', models.TextField()),
-                ('color', models.CharField(default='Black', max_length=50)),
-            ],
+            (
+                'id',
+                models.BigAutoField(
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
+                    verbose_name='ID'
+                )
+            ),
+    ('name', models.CharField(max_length=100)),
+    ('description', models.TextField()),
+    ('color', models.CharField(default='Black', max_length=50)),
+],
         ),
         migrations.CreateModel(
             name='CarModel',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID'
+                    )
+                ),
                 ('name', models.CharField(max_length=100)),
-                ('type', models.CharField(choices=[('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon'), ('HONDA', 'Honda')], default='SUV', max_length=10)),
-                ('year', models.IntegerField(default=2023, validators=[django.core.validators.MaxValueValidator(2023), django.core.validators.MinValueValidator(2015)])),
-                ('car_make', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='djangoapp.carmake')),
+                (
+                    'type',
+                    models.CharField(
+                        choices=[
+                            ('SEDAN', 'Sedan'),
+                            ('SUV', 'SUV'),
+                            ('WAGON', 'Wagon'),
+                            ('HONDA', 'Honda')
+                        ],
+                        default='SUV',
+                        max_length=10
+                    )
+                ),
+                (
+                    'year',
+                    models.IntegerField(
+                        default=2023,
+                        validators=[
+                            django.core.validators.MaxValueValidator(2023),
+                            django.core.validators.MinValueValidator(2015)
+                        ]
+                    )
+                ),
+                (
+                    'car_make',
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to='djangoapp.carmake'
+                    )
+                ),
             ],
         ),
     ]
